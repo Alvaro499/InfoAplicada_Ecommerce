c92c1a8bcb1bc71eb5616eb2746b08eb8049d4e1
Alvaro499 <alvarosiles499@gmail.com>
2025-11-24 15:10:36 -0600

    Con Driver-Autoinstaller

--- selenium-tests/utils/driver_factory.py
+++ selenium-tests/utils/driver_factory.py
@@ -1,16 +1,5 @@
-
-#Porción de codigo que se reutiliza para estar evitando llamando
-#al webdriver.Chrome() en cada test
-
-#from selenium import webdriver
-
-#def create_driver():
-#    driver = webdriver.Chrome()
-#    driver.maximize_window()
-#    return driver
-
-
 import os
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -20,14 +9,10 @@ def create_driver():
     running_in_ci = os.getenv("CI", "false").lower() == "true"
 
     if running_in_ci:
-        # Desactivar auto-descarga de Selenium
-        os.environ["SE_CHROMEDRIVER_SKIP_DOWNLOAD"] = "1"
-        os.environ["SE_SKIPPED_DRIVERS"] = "chromedriver"
-        os.environ["webdriver.chrome.driver"] = "/usr/bin/chromedriver"
-
-        # Flags para headless en Cloud Build
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+
+    chromedriver_autoinstaller.install()
 
     return webdriver.Chrome(options=options)
